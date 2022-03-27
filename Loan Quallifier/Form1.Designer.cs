6edbc3bafed220406117be7b3ef89e48b43983f4
Zac <Zac@RIPPLE-XRP>
2022-03-28 12:15:27 +1300

    Add project files.

---
+++ Loan Quallifier/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Loan_Quallifier
+{
+    partial class loanQualifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.yearsTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.decisionLabel = new System.Windows.Forms.Label();
+            this.loanDcisionLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkButton
+            // 
+            this.checkButton.BackColor = System.Drawing.Color.White;
+            this.checkButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.checkButton.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkButton.Location = new System.Drawing.Point(242, 351);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(150, 50);
+            this.checkButton.TabIndex = 0;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = false;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.White;
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clearButton.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(410, 351);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(150, 50);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(577, 351);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(150, 50);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Annual Salary:";
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaryTextBox.Location = new System.Drawing.Point(338, 131);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(300, 32);
+            this.salaryTextBox.TabIndex = 4;
+            // 
+            // yearsTextBox
+            // 
+            this.yearsTextBox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearsTextBox.Location = new System.Drawing.Point(338, 211);
+            this.yearsTextBox.Name = "yearsTextBox";
+            this.yearsTextBox.Size = new System.Drawing.Size(300, 32);
+            this.yearsTextBox.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox1.Controls.Add(this.loanDcisionLabel);
+            this.groupBox1.Controls.Add(this.decisionLabel);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.salaryTextBox);
+            this.groupBox1.Controls.Add(this.yearsTextBox);
+            this.groupBox1.Controls.Add(this.exitButton);
+            this.groupBox1.Controls.Add(this.checkButton);
+            this.groupBox1.Controls.Add(this.clearButton);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(958, 479);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loan Qualifier";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(335, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Years at current job:";
+            // 
+            // decisionLabel
+            // 
+            this.decisionLabel.AutoSize = true;
+            this.decisionLabel.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decisionLabel.Location = new System.Drawing.Point(334, 287);
+            this.decisionLabel.Name = "decisionLabel";
+            this.decisionLabel.Size = new System.Drawing.Size(0, 22);
+            this.decisionLabel.TabIndex = 7;
+            // 
+            // loanDcisionLabel
+            // 
+            this.loanDcisionLabel.AutoSize = true;
+            this.loanDcisionLabel.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loanDcisionLabel.Location = new System.Drawing.Point(336, 260);
+            this.loanDcisionLabel.Name = "loanDcisionLabel";
+            this.loanDcisionLabel.Size = new System.Drawing.Size(117, 22);
+            this.loanDcisionLabel.TabIndex = 8;
+            this.loanDcisionLabel.Text = "Loan Decision:";
+            // 
+            // loanQualifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(982, 503);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "loanQualifier";
+            this.Text = "Loan Qualifier";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox salaryTextBox;
+        private System.Windows.Forms.TextBox yearsTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label decisionLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label loanDcisionLabel;
+    }
+}
+
---
+++ Loan Quallifier/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loan_Quallifier
+{
+    public partial class loanQualifier : Form
+    {
+        public loanQualifier()
+        {
+            InitializeComponent();
+        }
+        //NESTED CONDITONAL STATEMENTS
+        private void checkButton_Click(object sender, EventArgs e)
+        { 
+            try
+            {
+                const decimal MINIMUM_SALARY = 40000m;
+                const decimal MINIMUM_YEARS_ON_JOB = 2;
+
+                //LOCAL VAROABLES
+                decimal salary;
+                int yearsOnJob;
+
+                //GET THE SALARY AND YEARS ON JOB
+                salary = decimal.Parse(salaryTextBox.Text);
+                yearsOnJob = int.Parse(yearsTextBox.Text);  
+
+                //DETERMINE WHETHER OR NOT THE USER QUALIFIES
+                if(salary >= MINIMUM_SALARY)
+                {
+                    if (yearsOnJob >= MINIMUM_YEARS_ON_JOB)
+                    {
+                        //THE USER QUALIFIES
+                        decisionLabel.Text="Congratulations! You qualify for the loan";
+                    }
+                    else
+                    {
+                        //THE USER DOES NOT QUALIFY
+                        decisionLabel.Text = "Sorry! "+"you do not qualify for the loan";
+                    }
+                }
+                else
+                {
+                //THE USER DOES NOT QUALIFY
+                decisionLabel.Text = "Minimum salary requirement " + "not met";
+                }
+
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //CLEAR TEXT BOXES
+            salaryTextBox.Clear();
+            yearsTextBox.Clear();
+            decisionLabel.Text = "";
+
+            //RESET THE FOCUS
+            salaryTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
